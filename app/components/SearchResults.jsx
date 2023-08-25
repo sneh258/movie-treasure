b340440a452bf3fec49bc0792c98f5cf5467e8ea
Sneha <snehalall.sneha@gmail.com>
2023-08-25 22:16:32 +0530

    feat: add search functionality

---
+++ app/components/SearchResults.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import Card from "./Card";
+
+function SearchResults({ searchText, movies }) {
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+  console.log(searchText)
+
+  useEffect(() => {
+    setFilteredMovies(movies);
+  }, [movies]);
+
+  return (
+    <div className="">
+        <div className="flex">
+            <h1>Top Search Results for &quot;{searchText}&quot;</h1>
+        </div>
+        <div className="flex-wrap flex justify-center gap-10 h-fit w-fit">
+            {filteredMovies.map((movie) => {
+                return <Card key={movie.id} movie={movie}></Card>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default SearchResults
